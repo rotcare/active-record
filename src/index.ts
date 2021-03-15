40b7af241384648a2f8b1c6a12516d8285c73110
Tao Wen <taowen@chengfayun.com>
2021-03-15 20:13:43 +0800

    add toRun

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * from './ActiveRecord';
 export * from './toGet';
 export * from './toQuery';
+export * from './toRun';
---
+++ src/toRun.ts
@@ -0,0 +1,13 @@
+import { Scene, Table } from "@rotcare/io";
+import { ActiveRecord } from './ActiveRecord';
+
+type MethodsOf<T> = {
+    [P in keyof T]: T[P] extends (...a: any) => any ? P : never;
+}[keyof T];
+
+export function toRun<T extends ActiveRecord, M extends MethodsOf<T>>(table: Table<T>, methodName: M) {
+    return async (scene: Scene, id: T['id'], ...args: Parameters<T[M]>): Promise<T[]> => {
+        const entity = await scene.get(table, id);
+        return await Reflect.get(entity, methodName).apply(entity, args);
+    }
+}

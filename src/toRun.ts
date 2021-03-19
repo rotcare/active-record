d05727cecb44a5b8ec58fe71960eaf4110123d48
Tao Wen <taowen@chengfayun.com>
2021-03-19 13:46:44 +0800

    run with scene

--- src/toRun.ts
+++ src/toRun.ts
@@ -8,6 +8,6 @@ type MethodsOf<T> = {
 export function toRun<T extends ActiveRecord, M extends MethodsOf<T>>(table: Table<T>, methodName: M) {
     return async (scene: Scene, id: T['id'], ...args: Parameters<T[M]>): Promise<T[]> => {
         const entity = await scene.get(table, id);
-        return await Reflect.get(entity, methodName).apply(entity, args);
+        return await Reflect.get(entity, methodName).call(entity, scene, ...args);
     }
 }

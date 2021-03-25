3bc29698a83fcccc557c1ef8862fd01bc795ca74
Tao Wen <taowen@chengfayun.com>
2021-03-25 16:16:54 +0800

    fix toRun infer type

--- src/toRun.ts
+++ src/toRun.ts
@@ -1,12 +1,14 @@
 import { Scene, Table } from "@rotcare/io";
 import { ActiveRecord } from './ActiveRecord';
 
+type OmitFirstArg<F> = F extends (x: any, ...args: infer P) => infer R ? (...args: P) => R : never;
+
 type MethodsOf<T> = {
     [P in keyof T]: T[P] extends (...a: any) => any ? P : never;
 }[keyof T];
 
 export function toRun<T extends ActiveRecord, M extends MethodsOf<T>>(table: Table<T>, methodName: M) {
-    return async (scene: Scene, id: T['id'], ...args: Parameters<T[M]>): Promise<T[]> => {
+    return async (scene: Scene, id: T['id'], ...args: Parameters<OmitFirstArg<T[M]>>): Promise<T[]> => {
         const entity = await scene.get(table, id);
         return await Reflect.get(entity, methodName).call(entity, scene, ...args);
     }
